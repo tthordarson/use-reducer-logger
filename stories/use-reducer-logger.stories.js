1b4a9148bcea1c885e35c5d01601f912cc4bd740
Thordur Thordarson <thor@eml.cc>
2020-11-04 17:44:34 -0500

    Setting up storybook to test the hook on a react component

---
+++ stories/use-reducer-logger.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SimpleDemo = () => {
+    return <div>TODO</div>
+}
+
+export default {
+    component: SimpleDemo,
+    title: 'useReducerLogger/SimpleDemo'
+}
